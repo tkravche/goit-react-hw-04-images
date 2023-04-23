98d094a2dd113b49f7ebea1ca6adc9860010a2ae
Tanya <tkravche@yahoo.com>
2023-04-23 12:54:30 +0200

    modal agian

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -5,18 +5,19 @@ import { useEffect } from 'react';
 
 export const Modal = ({ largeImage, onClose }) => {
   useEffect(() => {
+    const handleModalClose = e => {
+      if (e.key === 'Escape' || e.target === e.currentTarget) {
+        onClose();
+      }
+    };
     window.addEventListener('keydown', handleModalClose);
     return () => {
       window.removeEventListener('keydown', handleModalClose);
     };
-  }, []);
-  const handleModalClose = e => {
-    if (e.key === 'Escape' || e.target === e.currentTarget) {
-      onClose();
-    }
-  };
+  }, [onClose]);
+
   return (
-    <Overlay onClick={handleModalClose}>
+    <Overlay onClick={onClose}>
       <StyledModal>
         <img src={largeImage} alt="pic" />
       </StyledModal>
